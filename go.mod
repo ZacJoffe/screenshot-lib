15cc6f00d422d5806bd68556bb9d90c2f6a7bdf8
ZacJoffe <zacharyjoffe@gmail.com>
2019-04-14 16:19:06 -0400

    Used import command to take screenshot

--- go.mod
+++
@@ -1,5 +0,0 @@
-module screenshot
-
-go 1.12
-
-require gopkg.in/gographics/imagick.v3 v3.2.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-	_ "gopkg.in/gographics/imagick.v3/imagick"
+	"log"
+	"os/exec"
 )
 
-func main() {
+// checkCommand looks sees is a given command exists on the user's system, if the returned error is nil then it exists
+func checkCommand(command string) error {
+	_, err := exec.LookPath(command)
+	return err
+	/*
+		if err != nil {
+			return err
+		}
 
+		return nil
+	*/
+}
+
+func main() {
+	err := checkCommand("import")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := exec.Command("import", "screenshot.png")
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
