646b6a62a962a26485ec29185eb8afcc60860b05
ZacJoffe <zacharyjoffe@gmail.com>
2019-04-14 16:31:04 -0400

    Cleaned up code

--- main.go
+++ main.go
@@ -5,27 +5,38 @@ import (
 	"os/exec"
 )
 
+const outputDir = "/tmp/screenshot.png"
+
 // checkCommand looks sees is a given command exists on the user's system, if the returned error is nil then it exists
 func checkCommand(command string) error {
 	_, err := exec.LookPath(command)
 	return err
-	/*
-		if err != nil {
-			return err
-		}
+}
 
-		return nil
-	*/
+func ScreenshotSelect() error {
+	err := checkCommand("import")
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("import", outputDir)
+	return cmd.Run()
 }
 
 func main() {
-	err := checkCommand("import")
-	if err != nil {
-		log.Fatal(err)
-	}
+	/*
+		err := checkCommand("import")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	cmd := exec.Command("import", "screenshot.png")
-	err = cmd.Run()
+		cmd := exec.Command("import", outputDir)
+		err = cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
+	err := ScreenshotSelect()
 	if err != nil {
 		log.Fatal(err)
 	}
