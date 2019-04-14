a8c04c923b7ab2e3cfe408003890aa56bb0d85d2
ZacJoffe <zacharyjoffe@gmail.com>
2019-04-14 16:43:19 -0400

    Refactored functions into its own package, documented code

--- main.go
+++ main.go
@@ -1,52 +1,17 @@
 package main
 
 import (
+	"./imagemagick"
 	"log"
-	"os/exec"
 )
 
-const outputDir = "/tmp/screenshot.png"
-
-// checkCommand looks sees is a given command exists on the user's system, if the returned error is nil then it exists
-func checkCommand(command string) error {
-	_, err := exec.LookPath(command)
-	return err
-}
-
-func ScreenshotSelect() error {
-	err := checkCommand("import")
-	if err != nil {
-		return err
-	}
-
-	cmd := exec.Command("import", outputDir)
-	return cmd.Run()
-}
-
-func ScreenshotScreen() error {
-	err := checkCommand("import")
-	if err != nil {
-		return err
-	}
-
-	cmd := exec.Command("import", "-window", "root", outputDir)
-	return cmd.Run()
-}
-
 func main() {
-	/*
-		err := checkCommand("import")
-		if err != nil {
-			log.Fatal(err)
-		}
+	err := screenshot.Select()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		cmd := exec.Command("import", outputDir)
-		err = cmd.Run()
-		if err != nil {
-			log.Fatal(err)
-		}
-	*/
-	err := ScreenshotScreen()
+	err = screenshot.Screen()
 	if err != nil {
 		log.Fatal(err)
 	}
